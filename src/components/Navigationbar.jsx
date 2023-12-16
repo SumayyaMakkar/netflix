808aa6e0a362dafc1d04197edbe0026cdb1311c5
SumayyaMakkar <sumayyamakkar0009@gmail.com>
2023-12-16 13:27:46 -0800

    netflix india react project

---
+++ src/components/Navigationbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navigationbar.css'
+
+function Navigationbar() {
+  return (
+    
+    <div className='nav'>
+      <img width={'160px'} className='nav_image' src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
+    </div>
+  )
+}
+
+export default Navigationbar
